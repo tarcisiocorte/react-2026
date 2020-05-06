af63d3193ed6cad393790cc6abbb1f2caffcd64c
Rodrigo Manguinho <rodrigo.manguinho@gmail.com>
2020-05-06 15:50:00 -0300

    chore: config jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  moduleNameMapper: {
+    '^data/(.*)$': '<rootDir>/src/data/$1',
+    '^domain/(.*)$': '<rootDir>/src/domain/$1'
+  },
   transform: {
     '^.+\\.(ts|tsx)$': ['@swc/jest']
   }
