718136803e79a64ce9286e0103241c77ddf5ff21
Tarcísio Corte <tarcisiocorte@gmail.com>
2026-06-09 07:31:11 -0300

    feat: enhance HttpPostClientSpy to handle request body and add tests for RemoteAuthentication

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -2,9 +2,11 @@ import type { HttpPostClient, HttpPostParams } from 'data/protocols/http/http-po
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: object
 
   async post (params: HttpPostParams): Promise<void> {
     this.url = params.url
+    this.body = params.body
     return Promise.resolve()
   }
 }
