51e17eb4ba6af5ebc23f1dce7f361b6c9c3ab202
Tarcísio Corte <tarcisiocorte@gmail.com>
2026-06-10 08:22:57 -0300

    feat: add mockAccountModel function and update tests in RemoteAuthentication to return AccountModel on successful authentication

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,7 +4,7 @@ import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import type { AccountModel } from '@/domain/models/account-model'
-import { mockAuthentication } from '@/domain/test/mock-authentication'
+import { mockAccountModel, mockAuthentication } from '@/domain/test/mock-account'
 import type { AuthenticationParams } from '@/domain/usecases/authentication'
 import { RemoteAuthentication } from './remote-authentication'
 
@@ -15,6 +15,10 @@ type SutTypes = {
 
 const makeSut = (url = faker.internet.url()): SutTypes => {
   const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
+  httpPostClientSpy.response = {
+    statusCode: HttpStatusCode.ok,
+    body: mockAccountModel()
+  }
   const sut = new RemoteAuthentication(url, httpPostClientSpy)
 
   return {
@@ -84,5 +88,18 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(mockAuthentication())
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const httpResult = mockAccountModel()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+
+    const account = await sut.auth(mockAuthentication())
+
+    expect(account).toEqual(httpResult)
   })
 })
---
+++ src/domain/test/mock-account.ts
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker'
+import type { AccountModel } from '@/domain/models/account-model'
+import type { AuthenticationParams } from '@/domain/usecases/authentication'
+
+export const mockAuthentication = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.string.uuid()
+})
