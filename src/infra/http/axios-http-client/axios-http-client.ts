c54024af3de95619911c5aa4a5ebf680c4f8d155
Tarcísio Corte <tarcisiocorte@gmail.com>
2026-06-13 23:29:23 -0300

    fix: include request body in axios post call in AxiosHttpClient

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 export class AxiosHttpClient<T, R> implements HttpPostClient<T, R> {
   async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
-    await axios.post(params.url)
+    await axios.post(params.url, params.body)
     return {
       statusCode: HttpStatusCode.ok
     }
