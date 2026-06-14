4662e29a6f1b16eb27ff6724dee43fbdff6d0369
Tarcísio Corte <tarcisiocorte@gmail.com>
2026-06-13 23:28:35 -0300

    test: refactor AxiosHttpClient test to use mockPostRequest for request parameters

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,4 +1,5 @@
 import { faker } from '@faker-js/faker'
+import type { HttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 import { AxiosHttpClient } from './axios-http-client'
 
@@ -10,13 +11,20 @@ const makeSut = (): AxiosHttpClient<unknown, unknown> => {
   return new AxiosHttpClient()
 }
 
+const mockPostRequest = (): HttpPostParams<Record<string, unknown>> => ({
+  url: faker.internet.url(),
+  body: faker.helpers.objectValue({
+    value: faker.string.uuid()
+  })
+})
+
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct URL and verb', async () => {
-    const url = faker.internet.url()
+    const request = mockPostRequest()
     const sut = makeSut()
 
-    await sut.post({ url })
+    await sut.post(request)
 
-    expect(mockedAxios.post.mock.calls).toEqual([[url]])
+    expect(mockedAxios.post.mock.calls).toEqual([[request.url]])
   })
 })
