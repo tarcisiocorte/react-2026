7f6b2e45b0133708d7ed4fd5e31727d53fabdf74
Tarcísio Corte <tarcisiocorte@gmail.com>
2026-06-13 23:55:10 -0300

    feat: add Sass module declaration and update dependencies for improved styling support

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,13 +1,14 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^data/(.*)$': '<rootDir>/src/data/$1',
-    '^domain/(.*)$': '<rootDir>/src/domain/$1'
+    '^domain/(.*)$': '<rootDir>/src/domain/$1',
+    '\\.(css|scss|sass)$': 'identity-obj-proxy'
   },
   transform: {
     '^.+\\.[tj]sx?$': ['@swc/jest']
